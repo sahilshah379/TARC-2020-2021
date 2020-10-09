c7e02f2f0fc30a90f558d979c6e0640b35f747e7
Sahil Shah <sahilshah379@gmail.com>
2020-10-09 16:27:40 -0400

    ok

--- src/DecouplerTestBLE/DecouplerTestBLE.ino
+++ src/DecouplerTestBLE/DecouplerTestBLE.ino
@@ -1,5 +1,4 @@
 #include "Decoupler.h"
-#include <Servo.h>
 
 const int pin = 10;
 
@@ -11,21 +10,18 @@ const int max_degrees = 145;
 const int openPos = 30;
 const int closePos = 120;
 
-Servo myservo;
-
 Decoupler decoupler(refresh_interval, pulse_lower, pulse_upper, 0, max_degrees);
 
 void setup() {
   decoupler.link(pin, openPos, closePos);
-  myservo.attach(pin);
+  Serial.println("SetupCheckpoint");
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) {
-    myservo.write(pos);
-    delay(15);
-  }
-  for (pos = 180; pos >= 0; pos -= 1) {
-    myservo.write(pos);
-  }
+  decoupler.open();
+  Serial.println("OpenCheckpoint");
+  delay(1500);
+  decoupler.close();
+  Serial.println("CloseCheckpoint");
+  delay(1500);
 }
